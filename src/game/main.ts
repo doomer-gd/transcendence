460e9236bb0cd53dfa31d2f220b1cf75a0ac50e6
IgorKulik-es <igorkulik28@proton.me>
2026-05-13 16:48:17 +0200

    some experiments with movement

--- src/game/main.ts
+++ src/game/main.ts
@@ -4,6 +4,7 @@ import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import { TestScene } from './scenes/TestScene';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -18,8 +19,16 @@ const config: Phaser.Types.Core.GameConfig = {
         Preloader,
         MainMenu,
         MainGame,
+		TestScene,
         GameOver
-    ]
+    ],
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {y: 1300, x: 0},
+			debug: false
+		}
+	}
 };
 
 const StartGame = (parent: string) => {
---
+++ src/game/scenes/TestScene.ts
@@ -0,0 +1,52 @@
+import { EventBus } from '../EventBus';
+import { Scene } from 'phaser';
+
+export class TestScene extends Scene
+{
+	background: Phaser.GameObjects.Image;
+	player:		Phaser.Physics.Arcade.Sprite;
+	cursors:	Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+
+	constructor ()
+	{
+		super('TestScene');
+	}
+
+	preload ()
+	{
+		this.load.image('background_dark', './assets/samples/bulkhead-wallsx1.png');
+		this.load.image('player', './assets/samples/witch1.png');
+		this.load.image('enemy', './assets/samples/vampire1.png')
+	}
+
+	create()
+	{
+		this.background = this.add.image(500, 300 , 'background_dark');
+		this.background.setScale(2);
+		this.player = this.physics.add.sprite(700, 400, 'player');
+		this.player.setCollideWorldBounds(true);
+		this.cursors = this.input.keyboard?.createCursorKeys();
+	}
+
+	update(time: number, delta: number): void {
+		if (this.cursors?.left.isDown)
+		{
+			this.player.setVelocityX(-100);
+		}
+		else if (this.cursors?.right.isDown)
+		{
+			this.player.setVelocityX(100);
+		}
+		else
+		{
+			this.player.setVelocityX(0);
+		}
+		if (this.cursors?.space.isDown)// && this.player.body?.touching.down)
+		{
+			this.player.setVelocityY(-300);
+		}
+	}
+
+
+
+}
