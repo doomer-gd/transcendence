ff511c2dd8ebc9efe56a642b4d803967b5b1c18f
IgorKulik-es <igorkulik28@proton.me>
2026-05-22 17:03:07 +0200

    added new map with tiles and json

---
+++ config/maps/living_stone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="living_stone" tilewidth="32" tileheight="32" tilecount="54" columns="9">
+ <image source="../../assets/tiles/tileset1.png" width="288" height="192"/>
+</tileset>
--- src/game/scenes/TestScene.ts
+++ src/game/scenes/TestScene.ts
@@ -17,10 +17,16 @@ export class TestScene extends Scene
 		this.load.image('background_dark', './assets/samples/bulkhead-wallsx1.png');
 		this.load.image('player', './assets/samples/witch1.png');
 		this.load.image('enemy', './assets/samples/vampire1.png');
+    this.load.tilemapTiledJSON('cave', './config/maps/cave.json');
+    this.load.image('tiles', './assets/tiles/tileset1.png');
+    this.load.image('lava-background', './assets/tiles/lava-background.png');
 	}
 
 	create()
 	{
+    const map = this.make.tilemap({key: 'cave'});
+    const tileset = this.add.tileSprite;
+
 		this.background = this.add.image(500, 300 , 'background_dark');
 		this.background.setScale(2);
     if (this.matter.world.walls.bottom?.label)
