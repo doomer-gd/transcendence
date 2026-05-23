b99ab956c0ee24df92fba7a07b390c706906e43a
IgorKulik-es <igorkulik28@proton.me>
2026-05-23 22:05:05 +0200

    added new tilemap to scene

--- src/game/scenes/TestScene.ts
+++ src/game/scenes/TestScene.ts
@@ -1,11 +1,13 @@
 import { Player } from '../player/Player';
 import { Scene } from 'phaser';
+import { Constructors } from '../utility/Constructors';
 
 export class TestScene extends Scene
 {
 	background: Phaser.GameObjects.Image;
 	player:     Player;
   playerSprite: Phaser.Physics.Arcade.Sprite;
+  map: Phaser.Tilemaps.Tilemap;
 
 	constructor ()
 	{
@@ -19,16 +21,18 @@ export class TestScene extends Scene
 		this.load.image('enemy', './assets/samples/vampire1.png');
     this.load.tilemapTiledJSON('cave', './config/maps/cave.json');
     this.load.image('tiles', './assets/tiles/tileset1.png');
-    this.load.image('lava-background', './assets/tiles/lava-background.png');
+    this.load.image('lava-bg', './assets/tiles/lava-background.png');
+    this.load.json('keymap', '../../config/keymap.json');
 	}
 
 	create()
 	{
-    const map = this.make.tilemap({key: 'cave'});
-    const tileset = this.add.tileSprite;
-
-		this.background = this.add.image(500, 300 , 'background_dark');
-		this.background.setScale(2);
+    const back = this.add.image(0,0,'lava-bg').setOrigin(0,0);
+    back.setScale(1024 / back.width, 768 / back.height);
+    this.map = this.make.tilemap({key: 'cave'});
+    const tileset = this.map.addTilesetImage('living_stone', 'tiles');
+    this.map.createLayer('ground', tileset as Phaser.Tilemaps.Tileset);
+    this.map.getObjectLayer('boxes')?.objects.forEach((obj) => Constructors.initTileBoxes(this, obj, 'ground'));
     if (this.matter.world.walls.bottom?.label)
        this.matter.world.walls.bottom.label = 'ground';
     this.player = new Player(this, 400, 300);
