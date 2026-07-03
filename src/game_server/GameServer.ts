212df3c32e49f8d07e5b5fe2642f5edb0ea51a86
IgorKulik-es <igorkulik28@proton.me>
2026-07-03 20:47:57 +0200

    added socket.io subserver, removed placeholders

--- src/game_server/GameServer.ts
+++ src/game_server/GameServer.ts
@@ -1,37 +1,23 @@
-import { Server, Socket } from "socket.io"
+import { Server } from "socket.io"
 import { createServer } from "http"
 
 
-const port: number = 8081; //defalut
+const port: number = 3000;
+
 const server = createServer();
 const ioSock = new Server(server,{
     cors: {
         origin: "*"
     }
 });
-
-export interface SampleData
-{
-  x: number;
-  y: number;
-  color: string;
-}
-
 server.listen(port);
-console.log("Server started at:", port);
 
 ioSock.on("connection", (socket) => {
   console.log(socket.id, "joined");
+
   socket.on("disconnect", () => {
     console.log(socket.id, "disconnected");
   });
-
-  socket.on("hello", (text) => {
-    console.log(`client ${socket.id}: `, text);
-  });
-  socket.on("data", (data: SampleData) =>{
-    console.log("x:", data.x, "y:", data.y, "color:", data.color);
-  })
 });
 
 
